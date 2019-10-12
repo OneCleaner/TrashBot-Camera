2473f35a0a8126a43d1e7708939d3f9b959fb108
Gabriele Onorato <lapry34@gmail.com>
2019-10-12 12:01:34 +0200

    Identazione
    @alby7503 controllala

--- camera_skt.py
+++ camera_skt.py
@@ -3,13 +3,13 @@
 from base64 import b64encode
 from socket import AF_INET, SOCK_STREAM, gethostbyname, gethostname, socket, SOL_SOCKET, SO_REUSEADDR
 from threading import Thread
-
+from time import sleep
 from cv2 import VideoCapture, imencode, imshow
 
 SOCK = socket(AF_INET, SOCK_STREAM)
 SOCK.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 #HOST = gethostbyname(gethostname())
-HOST = "192.168.1.101"
+HOST = "127.0.0.1"
 PORT = int(input("Port: "))
 SOCK.bind((HOST, PORT))
 print(f"{HOST}:{PORT}")
@@ -25,40 +25,40 @@ STREAM1.set(4, 240)
 #STREAM0 = STREAM1
 
 def server():
-"""Stream live video"""
-ret0, frame0 = STREAM0.read()
-ret1, frame1 = STREAM1.read()
-try:
-retval0, buffer0 = imencode('.jpg', frame0)
-retval1, buffer1 = imencode('.jpg', frame1)
-server_thread = Thread(target=server, daemon=True)
-conn, addr = SOCK.accept()
-server_thread.start()
-except Exception:
-return
-#imshow("frame0", frame0)
-#imshow("frame1", frame1)
-try:
-data = conn.recv(512).decode().split(' ')
-if data[0] == "GET":
-with open("stream.html", "rb") as website:
-conn.sendall(b"HTTP/1.1 200 OK\n\n" + website.read())
-elif data[0] == "VIDEO0":
-conn.sendall(b"HTTP/1.1 200 OK\n\n" + b64encode(buffer0))
-elif data[0] == "VIDEO1":
-conn.sendall(b"HTTP/1.1 200 OK\n\n" + b64encode(buffer1))
-except ConnectionResetError:
-return
-conn.close()
+    """Stream live video"""
+    ret0, frame0 = STREAM0.read()
+    ret1, frame1 = STREAM1.read()
+    try:
+        retval0, buffer0 = imencode('.jpg', frame0)
+        retval1, buffer1 = imencode('.jpg', frame1)
+        server_thread = Thread(target=server, daemon=True)
+        conn, addr = SOCK.accept()
+        server_thread.start()
+    except Exception:
+        return
+    #imshow("frame0", frame0)
+    #imshow("frame1", frame1)
+    try:
+        data = conn.recv(512).decode().split(' ')
+        if data[0] == "GET":
+            with open("stream.html", "rb") as website:
+                conn.sendall(b"HTTP/1.1 200 OK\n\n" + website.read())
+        elif data[0] == "VIDEO0":
+            conn.sendall(b"HTTP/1.1 200 OK\n\n" + b64encode(buffer0))
+        elif data[0] == "VIDEO1":
+            conn.sendall(b"HTTP/1.1 200 OK\n\n" + b64encode(buffer1))
+    except ConnectionResetError:
+        return
+    conn.close()
 
 
-server()
-from time import sleep
-while True:
-try:
-sleep(20)
-except KeyboardInterrupt:
-SOCK.close()
-break
-STREAM0.release()
-STREAM1.release()
+if __name__ == "__main__":
+    server()
+    while True:
+        try:
+            sleep(20)
+        except KeyboardInterrupt:
+            SOCK.close()
+        break
+    STREAM0.release()
+    STREAM1.release()
